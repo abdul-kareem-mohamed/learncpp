551cec68df7bcdf085d23b819c6db38cfbd098ec
Abdul Kareem Mohamed <118671819+abdul-kareem-mohamed@users.noreply.github.com>
2026-03-31 16:11:18 +0200

    Refactor readsAddsTwoNums.cpp to remove unused code
    Removed unnecessary iostream inclusion and simplified function calls.

--- C++ Basics: Functions and Files/readsAddsTwoNums.cpp
+++ C++ Basics: Functions and Files/readsAddsTwoNums.cpp
@@ -1,22 +1,13 @@
-#include <iostream>
+// We don't need to #include <iostream> since readsAddsTwoNums.cpp doesn't use any input/output functionality
 
-int readNumber()
-{
-    std::cout << "Enter a number to add: ";
-    int x {};
-    std::cin >> x;
-    return x;
-}
-
-void writeAnswer(int x)
-{
-    std::cout << "The answer is " << x << '\n';
-}
+// These are the forward declarations for the functions in io.cpp
+int readNumber();
+void writeAnswer(int x);
 
 int main()
 {
     int x { readNumber() };
     int y { readNumber() };
-    writeAnswer(x + y); // using operator+ to pass the sum of x and y to writeAnswer()
+    writeAnswer(x+y);
     return 0;
 }
