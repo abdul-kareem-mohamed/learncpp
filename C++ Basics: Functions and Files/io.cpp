ff2ea25024b435de864d4a09672c0a05a052aa3f
Abdul Kareem Mohamed <118671819+abdul-kareem-mohamed@users.noreply.github.com>
2026-03-31 16:11:46 +0200

    Add functions to read a number and write an answer

---
+++ C++ Basics: Functions and Files/io.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int readNumber()
+{
+    std::cout << "Enter a number to add: ";
+    int x {};
+    std::cin >> x;
+    return x;
+}
+
+void writeAnswer(int x)
+{
+    std::cout << "The answer is " << x << '\n';
+}
